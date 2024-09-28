b8cc132a0f62e9f783dc2addd2acad7a8bd93c63
MerlinGeorg <merlingeo96@gmail.com>
2024-09-28 00:12:48 -0400

    GET & POST request handling

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const app = express();
 app.use(express.json())
 
 let datastore = {
-    users: []
+    products: []
 }
 
+
+app.get('/products', (req,res)=>{
+    res.json(datastore.products)
+})
+
+app.post('/product', (req, res)=>{
+    const productData = req.body;
+    if(!productData || !productData.productName || !productData.quantity){
+        return res.status(400).json({error:"Invalid Data"});
+    }
+    datastore.products.push(userData);
+    res.status(201).json(productData)
+})
